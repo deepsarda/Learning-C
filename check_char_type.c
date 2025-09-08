4f5293df0bf61442b7fd935356ebda34b598d857
Deep Sarda <deep.p.sarda@gmail.com>
2025-09-08 12:29:24 +0530

    chore: improve comments and format code

--- check_char_type.c
+++ check_char_type.c
@@ -1,20 +1,27 @@
-// Check if given character the case of a letter. If it is letter, digit or symbol.
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
+int main()
+{
 
     char c;
     printf("Enter a character: ");
     scanf("%c", &c);
 
     // ASCII code for capital letters is from 65-90
-    if(c >= 65 && c <= 90){
+    if (c >= 65 && c <= 90)
+    {
         printf("%c is a capital letter.\n", c);
-    }else if(c >= 97 && c <= 122){  // ASCII code for small letters
+    }
+    else if (c >= 97 && c <= 122)
+    { // ASCII code for small letters
         printf("%c is a small letter.\n", c);
-    }else if(c >= 48 && c <= 57){ // ASCII code for digits
+    }
+    else if (c >= 48 && c <= 57)
+    { // ASCII code for digits
         printf("%c is a digit.\n", c);
-    }else{ // Assume rest all to be symbols. 
+    }
+    else
+    { // We assume rest all to be symbols.
         printf("%c is a symbol.\n", c);
     }
 
